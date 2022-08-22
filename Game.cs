c16922252cdf8d09d476b4798e8de2fa822d3c94
Lodis <lodis.perkins@aie.edu>
2022-08-22 16:54:51 -0500

    Added welcome screen

--- Game.cs
+++ Game.cs
@@ -5,9 +5,29 @@ using System.Text;
 namespace HelloWorld
 {
     class Game
-    {
+    { 
         public void Run()
         {
+            
+            //Initialize base stats.
+            string name = "";
+            float health = 100;
+            bool isAlive = true;
+            float damage = 10;
+
+            //Assigns name variable based on user input.
+            Console.WriteLine("Please input a name.");
+            name = Console.ReadLine();
+
+            //Prints all stats of current player.
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Stats");
+            Console.Write("Name: ");
+            Console.WriteLine(name);
+            Console.Write("Health: ");
+            Console.WriteLine(health);
+            Console.Write("Damage: ");
+            Console.WriteLine(damage);
 
         }
     }
